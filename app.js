65fe40a3919832731d46c1ff057bb0a380ec383b
ruchikxxii <ruchik710@gmail.com>
2022-08-16 12:42:20 +0530

    fixed time error

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ const formatMessage = (username,content) => {
     return ({
         username: username,
         content: content,
-        time: moment().format('h:mm a')
+        time: moment().utcOffset("+5:30").format('h:mm a')
     });
 }
